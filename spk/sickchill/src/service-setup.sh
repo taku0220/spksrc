1c1486c39563e6d2a14d824cb66593fe7ec36973
miigotu <miigotu@gmail.com>
2021-04-02 13:15:18 -0400

    sickchill: fix upgrade and remove cross, set update_frequency to 24 hours
    Signed-off-by: miigotu <miigotu@gmail.com>

--- spk/sickchill/src/service-setup.sh
+++ spk/sickchill/src/service-setup.sh
@@ -12,19 +12,21 @@ SC_CFG_FILE="${SC_DATA_DIR}/config.ini"
 
 GROUP="sc-download"
 
-SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
+SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR} --port 8081"
 
 set_config() {
-    . ${SYNOPKG_PKGDEST}/share/git_data
-
     if [ -f "${SC_CFG_FILE}" ]; then
         if [ -n "${wizard_username}" ] && [ -n "${wizard_password}" ]; then
             sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
             sed -i "/^\s*web_password\s*=/s/\s*=\s*.*/ = ${wizard_password}/" ${SC_CFG_FILE}
         fi
-        sed -i "/^\s*branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_BRANCH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_hash\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_HASH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_BRANCH}/" ${SC_CFG_FILE}
+        # postupgrade we don't want to change their branch or commit, just remove them as the updater should repopulate these
+        sed -i "/^branch/d" ${SC_CFG_FILE}
+        sed -i "/^cur_commit_hash/d" ${SC_CFG_FILE}
+        sed -i "/^cur_commit_branch/d" ${SC_CFG_FILE}
+
+        # Make sure we always update the port in their restored config
+        sed -i "s/\(web_port *= *\).*/\18081/" ${SC_CFG_FILE}
     else
         mkdir -p ${SC_DATA_DIR}
         cat << EOF > ${SC_CFG_FILE}
@@ -32,9 +34,18 @@ set_config() {
 web_username = ${wizard_username}
 web_password = ${wizard_password}
 branch = ${SC_GIT_BRANCH}
-cur_commit_hash = ${SC_GIT_COMMIT_HASH}
-cur_commit_branch = ${SC_GIT_COMMIT_BRANCH}
+web_port = 8081
+update_frequency = 24
 EOF
+    fi
+}
+
+service_preinst ()
+{
+    # Avoid ssl errors in git
+    SSL_CRT="/etc/ssl/certs/ca-certificates.crt"
+    if [ -f ${SSL_CRT} ]; then
+        ${GIT} config --system http.sslCAInfo ${SSL_CRT} > /dev/null 2>&1
     fi
 }
 
@@ -51,5 +62,7 @@ service_postinst() {
 }
 
 service_postupgrade() {
+    # Do we need to do this?
+    # chown -R sc-download:sc-download ${SC_DATA_DIR}
     set_config
 }
